afafca64bea656e000b4e50bdfe369b399c34bf5
mpoelst <mpoelstr@chicagobooth.edu>
2015-06-09 22:39:43 -0500

    update validations

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,6 +2,7 @@ class Day < ActiveRecord::Base
 validates :origin_location_id, :presence => true
 validates :destination_location_id, :presence => true
 validates :itinerary_id, :presence => true
+validates :arrival_date, :presence => true
 belongs_to :itinerary, :class_name => "Itinerary", :foreign_key => "itinerary_id"
 belongs_to :origin_location, :class_name => "Location", :foreign_key => "origin_location_id"
 belongs_to :destination_location, :class_name => "Location", :foreign_key => "destination_location_id"
