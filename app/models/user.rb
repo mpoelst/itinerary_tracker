8210371917c9e9fda1ae0e65ef13324cd7e69fa6
mpoelst <mpoelstr@chicagobooth.edu>
2015-06-10 22:44:18 -0500

    login information

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true
   has_many :itineraries
   has_many :locations, :through => :days
   has_many :days, :through => :itineraries
